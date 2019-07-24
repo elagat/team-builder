7167838fd66d83f6bdaac7c49db6452699cbefe5
Emily Lagattolla <elagattolla@gmail.com>
2019-07-24 16:08:14 -0500

    displayed form elements

---
+++ team-builder/src/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Form() {
+  return (
+    <div className="Form">
+      <form>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
