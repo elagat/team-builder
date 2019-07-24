eda26bf36bf202dd47c771d82cb3fc296182b92a
Emily Lagattolla <elagattolla@gmail.com>
2019-07-24 17:53:41 -0500

    WIP: displaying Member List

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
 import Form from './Form.js';
+import MemberList from "./MemberList";
 
 import './App.css';
 
 function App() {
-  const [memberList, setMemberList] = useState([]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <MemberList />
     </div>
   );
 }
